928cdd7f78ffb96c9d8d336cbfc13574277fb850
Alex Mwangi <alex@stl-horizon.com>
2022-11-16 22:21:44 +0300

    Video project

---
+++ JS-DOM-PROJECTS/09-Video/app.js
@@ -0,0 +1,24 @@
+// MDN
+// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
+// The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+
+
+// select switch buttons and video container
+
+const switchBtnElmnt = document.querySelector('.switch-btn')
+const videoContElmnt = document.querySelector('.video-container')
+
+// Event Listeners
+//Toggle slide for play/pause
+
+switchBtnElmnt.addEventListener('click',function(){
+    if(!switchBtnElmnt.classList.contains('slide')){
+        switchBtnElmnt.classList.add('slide')
+        // pause video
+        videoContElmnt.pause()
+    }else{
+        switchBtnElmnt.classList.remove('slide')
+        // play video
+        videoContElmnt.play()
+    }
+})
