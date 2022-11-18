11e92e8e87f2a723a158b222e58557bab4f412d7
Alex Mwangi <alex@stl-horizon.com>
2022-11-18 21:25:27 +0300

    GroceryBud Project - addItem Setup

---
+++ JS-DOM-PROJECTS/14-Grocery-bud/app.js
@@ -0,0 +1,49 @@
+
+// ****** SELECT ITEMS **********
+
+const alertParagraphElmnt = document.querySelector('.alert')
+const formElmnt = document.querySelector('.grocery-form')
+const groceryInputElmnt = document.getElementById('grocery')
+const groceryContElmnt = document.querySelector('.grocery-container')
+const groceryListElmnt = document.querySelector('.grocery-list')
+const editBtnElmnt = document.querySelector('.edit-btn')
+const deleteBtnElmnt = document.querySelector('.delete-btn')
+
+const clearBtnElmnt = document.querySelector('.clear-btn')
+
+
+
+// edit option
+let editElement
+let isEditing = false
+let editId = ''
+
+
+// ****** EVENT LISTENERS **********
+// submit form
+
+formElmnt.addEventListener('submit',addItem)
+
+
+// ****** FUNCTIONS **********
+
+function addItem(e){
+    e.preventDefault();
+    const groceryValue = groceryInputElmnt.value
+    const id = new Date().getTime().toString()
+    
+    if(groceryValue !== '' && isEditing === false){
+        console.log('add item to the list');
+    }
+    else if(groceryValue !== '' && isEditing === true){
+        console.log('editing');
+    }
+    else{
+        console.log('empty values');
+    }
+    
+}
+
+// ****** LOCAL STORAGE **********
+
+// ****** SETUP ITEMS **********
