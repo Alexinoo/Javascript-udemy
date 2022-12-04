e73ed45e3ab431d36e1e2aea4453bb30becc5839
Alex Mwangi <alex@stl-horizon.com>
2022-12-04 12:23:43 +0300

    Dark Mode Project - Toggle Dark / White theme CSS Functionality

--- JS-PROJECTS/02-dark-mode/app.js
+++ JS-PROJECTS/02-dark-mode/app.js
@@ -1,1 +1,9 @@
-console.log('dark mode');
+
+// select toggle button
+
+const toggleBtnElmnt = document.querySelector('.btn')
+
+toggleBtnElmnt.addEventListener('click',()=>{    
+    // console.log(document.documentElement);//html
+    document.documentElement.classList.toggle('dark-theme')
+})
