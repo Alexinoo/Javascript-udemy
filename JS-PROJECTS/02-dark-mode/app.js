c9889b3614d1b7ac6e32fc3afe5a1077718a9ff1
Alex Mwangi <alex@stl-horizon.com>
2022-12-04 12:48:46 +0300

    Dark Mode Project - Import articles and render articles in HTML

--- JS-PROJECTS/02-dark-mode/app.js
+++ JS-PROJECTS/02-dark-mode/app.js
@@ -1,9 +1,34 @@
 
 // select toggle button
-
 const toggleBtnElmnt = document.querySelector('.btn')
 
+// toggle dark-mode / white on click event
 toggleBtnElmnt.addEventListener('click',()=>{    
-    // console.log(document.documentElement);//html
+    
+    // select root element(html)
+    // toggle .dark-theme class
     document.documentElement.classList.toggle('dark-theme')
 })
+
+// import '.data/js
+// add to html <script src="./data.js"></script>
+
+// select section.articles
+// loop over each article and output to section.articles
+const articleSectionElmnt = document.querySelector('.articles')
+
+const articleElmntArr = articles.map(article => {
+
+    const { title , date , length ,snippet } = article
+    return `
+          <article class="post">
+           <h2>${title}</h2>
+           <div class="post-info">
+            <span>${date}</span>
+            <span>${length} min read</span>
+           </div>
+           <p>${snippet}</p>
+        </article>`
+}).join('')
+
+articleSectionElmnt.innerHTML = articleElmntArr
