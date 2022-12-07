99fc6e07ac6aad93898499e8d633903a69783c91
Alex Mwangi <alex@stl-horizon.com>
2022-12-07 13:21:40 +0300

    Dad Jokes Project - Selec Elements - Listen for click event

---
+++ JS-AJAX-PROJECTS/01-Dad-jokes/app.js
@@ -0,0 +1,12 @@
+
+// Dad Jokes URL -Endpoint
+const url = 'https://icanhazdadjoke.com/'
+
+const btnElmnt = document.querySelector('.btn')
+const pElmnt = document.querySelector('.result')
+
+// console.log(btnElmnt,pElmnt);
+
+btnElmnt.addEventListener('click',()=>{
+    
+})
