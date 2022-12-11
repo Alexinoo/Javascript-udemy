8df8a5a87dc031ab032861b118092fa3fdcff97a
Alex Mwangi <alex@stl-horizon.com>
2022-12-11 15:54:49 +0300

    Products project : Fetch Product

--- JS-AJAX-PROJECTS/02-Products/product.js
+++ JS-AJAX-PROJECTS/02-Products/product.js
@@ -1,16 +1,26 @@
 
-const url = 'https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog'
+const url = 'https://course-api.com/javascript-store-single-product'
 
 const prodSecElmnt = document.querySelector('.product')
 
 const fetchProduct = async()=>{
-    try {        
-        const response = await fetch(url)
+
+    prodSecElmnt.innerHTML = `<h4 class="product-loading">Loading...</h4>`   
+    
+    // get id dynamic
+    const params = new URLSearchParams(window.location.search)
+    const id = params.get('id')
+
+    try { 
+        const response = await fetch(`${url}?id=${id}`) 
+        if(!response.ok){
+            throw new Error('resource not found...')
+        }
         const data = await response.json()
         return data
 
     } catch (error) {
-        console.log(error);
+         prodSecElmnt.innerHTML =`<p class="error">There was a problem loading the product. Please try again later!</p>`
     }
 
 }
