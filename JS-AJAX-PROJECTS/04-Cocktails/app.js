65793fbc9fc30e99c3a09a9655eebc62e2c59ebc
Alex Mwangi <alex@stl-horizon.com>
2022-12-11 20:40:19 +0300

    Imported searchForm - Though not doing anything with it in particular- Just to run it

--- JS-AJAX-PROJECTS/04-Cocktails/app.js
+++ JS-AJAX-PROJECTS/04-Cocktails/app.js
@@ -1,7 +1,8 @@
 import presentDrinks from './src/presentDrinks.js'
+import './src/searchForm.js'
 
 // search cocktails by some query params
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=martini'
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a'
 
 // single cocktail drink
 // https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007
